def849dc1eb88228c9dc7b62088547931957b18b
Anjali-Pradeepu <anjalipradeepu@gmail.com>
2022-07-18 22:47:49 +0530

    commit

---
+++ tuple.py
@@ -0,0 +1,34 @@
+#Tuple
+t=8,7,1,2,3,4,5,1 #paranthesis is optional , it will create tuple if there is no brackets 
+print(t)
+
+#count function
+c=t.count(1)
+print(c)
+
+#index function
+res=t.index(4)
+print(res)
+
+#min function
+mint=min(t)
+print(mint)
+
+#max function
+maxt=max(t)
+print(maxt)
+
+#sort function
+tsort=sorted(t)
+print(tsort)
+
+
+#TUPLE packing
+a=10
+b=20
+c=30
+tpack=a,b,c
+print(tpack)
+
+
+
